334ece8b4c0f3812cea19e31c2325075b9ce4114
Polina Stavro <juggernautt@Polinas-MacBook-Pro.local>
2019-03-19 17:33:45 +0200

    logger init

---
+++ src/Logger.php
@@ -0,0 +1,115 @@
+<?php
+namespace Roxby\Logger;
+
+/**
+ * Logging client.
+ * Currently uses logz.io as service
+ */
+class RoxbyLogger
+{
+    private $apiKey = null;
+    private $application = null; // app identifier
+    public static function getInstance()
+    {
+        static $instance = null;
+        if (null === $instance) {
+            $instance = new static();
+        }
+        return $instance;
+    }
+    public function setApiKey($apiKey)
+    {
+        $this->apiKey = $apiKey;
+    }
+    public function setApplication($application)
+    {
+        $this->application = $application;
+    }
+    public function critical($message, array $data = [])
+    {
+        $this->log('critical', $message, $data);
+    }
+    public function error($message, array $data = [])
+    {
+        $this->log('error', $message, $data);
+    }
+    public function info($message, array $data = [])
+    {
+        $this->log('info', $message, $data);
+    }
+    public function debug($message, array $data = [])
+    {
+        $this->log('debug', $message, $data);
+    }
+    /**
+     * Add the log to list or send now
+     *
+     * @param string $type The log type
+     * @param string $message The log message
+     * @param array $data Additional data
+     * @throws \Exception
+     */
+    private function log($type, $message, array $data)
+    {
+        $log = $this->makeLog($message, $data);
+        $this->sendLog($log, $type);
+    }
+    /**
+     * Send the log to Logz.io
+     *
+     * @param $log
+     * @param string $type The log type
+     * @return \Psr\Http\Message\StreamInterface
+     * @throws \Exception
+     */
+    private function sendLog($log, $type = '')
+    {
+        $this->validateSendLog();
+        $response = $this->getClient()->post('/', [
+            'query' => [
+                'token' => $this->apiKey,
+                'type' => $type
+            ],
+            'body' => $log
+        ]);
+        return $response->getBody();
+    }
+    /**
+     * Format the log to send
+     *
+     * @param string $message The log message
+     * @param array $data Additional data
+     * @return false|string
+     */
+    private function makeLog($message, array $data = [])
+    {
+        return json_encode(
+            array_merge(
+                [
+                    'message' => $message,
+                    'application' => $this->application
+                ],
+                $data
+            )
+        );
+    }
+    /**
+     * Validate if log can be sent
+     * @throws \Exception
+     */
+    private function validateSendLog()
+    {
+        if (!$this->apiKey || !$this->application) {
+            throw new \Exception('You must specify Logz.io api key and your application');
+        }
+    }
+    private function getClient()
+    {
+        return new \GuzzleHttp\Client([
+            'base_uri' => 'https://listener.logz.io:8071'
+        ]);
+    }
+    private function __construct()
+    {
+    }
+}
